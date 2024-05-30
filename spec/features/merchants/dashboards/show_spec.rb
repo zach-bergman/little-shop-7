58393a77418626fe63693ea6a5fdde57a52d0be1
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-05-29 18:15:14 -0600

    test: links to merchants items and invoices

--- spec/features/merchants/dashboards/show_spec.rb
+++ spec/features/merchants/dashboards/show_spec.rb
@@ -18,5 +18,16 @@ RSpec.describe 'Dashboard' do
       expect(page).to have_content(@merchant2.name)
       expect(page).to_not have_content(@merchant1.name)
     end
+
+    it 'contains links to the items and invoices' do
+      visit merchant_dashboard_index_path(@merchant1.id)
+
+      expect(page).to have_link(merchant_items_index_path(@merchant1))
+      expect(page).to have_link(merchant_invoices_index_path(@merchant1))
+      # As a merchant,
+      # When I visit my merchant dashboard (/merchants/:merchant_id/dashboard)
+      # Then I see link to my merchant items index (/merchants/:merchant_id/items)
+      # And I see a link to my merchant invoices index (/merchants/:merchant_id/invoices)
+    end
   end
 end
