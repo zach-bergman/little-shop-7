a2d91ad5c05702069db38fc461b1776571a6edc8
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-05-31 15:45:13 -0600

    test: invoice relationships

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,11 +1,13 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
-  describe "relationships" do
-    it { should belong_to :customer}
+  describe 'relationships' do
+    it { should belong_to :customer }
+    it { should have_many :invoice_items }
+    it { should have_many(:items).through(:invoice_items) }
   end
 
-  describe "enums" do
-    it { should define_enum_for(:status).with_values({ "in progress" => 0, "completed" => 1, "cancelled" => 2}) }
+  describe 'enums' do
+    it { should define_enum_for(:status).with_values({ 'in progress' => 0, 'completed' => 1, 'cancelled' => 2 }) }
   end
 end
