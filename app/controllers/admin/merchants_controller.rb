8cc6083676aec86db5e25b240f049341b8e50faa
Zach Bergman <bzach634@gmail.com>
2024-06-01 11:00:40 -0600

    feat: User Story 26

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -6,4 +6,22 @@ class Admin::MerchantsController < ApplicationController
   def show
     @merchant = Merchant.find(params[:id])
   end
+
+  def edit
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def update
+    merchant = Merchant.find(params[:id])
+
+    if merchant.update!(merchant_params)
+      flash[:notice] = "#{merchant.name}'s info updated successfully!"
+      redirect_to admin_merchant_path(merchant)
+    end
+  end
+
+  private
+  def merchant_params
+    params.permit(:name)
+  end
 end
