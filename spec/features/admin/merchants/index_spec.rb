1d88ef93f73afdcea962de56938478b1bbe5f993
Zach Bergman <bzach634@gmail.com>
2024-06-01 08:17:28 -0600

    feat: US25

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.describe "Admin Merchants Index" do
   before(:each) do
     @merchants = create_list(:merchant, 5)
+    @merchant_test = create(:merchant)
 
     visit admin_merchants_path
   end
@@ -15,6 +16,22 @@ RSpec.describe "Admin Merchants Index" do
             expect(page).to have_content(merchant.name)
           end
         end
+      end
+    end
+
+    describe "User Story 25" do
+      it "shows each merchant's name as a link" do
+        within("div#merchant_names") do
+          @merchants.each do |merchant|
+            expect(page).to have_link(merchant.name)
+          end
+        end
+      end
+
+      it "directs to the admin show page when a merchant's name is clicked" do
+        click_link(@merchant_test.name)
+
+        expect(current_path).to eq(admin_merchant_path(@merchant_test.id))
       end
     end
   end
