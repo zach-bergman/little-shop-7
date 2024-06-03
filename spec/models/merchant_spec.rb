9aaeaeb44800f699477c9afe98522e241ea36215
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-06-03 10:15:39 -0600

    test: model test for top five items

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -77,7 +77,7 @@ RSpec.describe Merchant, type: :model do
   end
 
   describe 'enums' do
-    it { should define_enum_for(:status).with_values({ disabled: 0, enabled: 1}) }
+    it { should define_enum_for(:status).with_values({ disabled: 0, enabled: 1 }) }
   end
 
   describe 'instance methods' do
@@ -85,6 +85,48 @@ RSpec.describe Merchant, type: :model do
       it 'returns the top five customers who had most successful transcations for merchant' do
         expect(@merchant_1.top_five_customers).to eq([@customer_1, @customer_4, @customer_5, @customer_2, @customer_3])
         expect(@merchant_1.top_five_customers).not_to include(@customer_6)
+      end
+    end
+    describe 'top five items' do
+      before do
+        @merchant_1 = create(:merchant)
+
+        @item_1 = create(:item, merchant: @merchant_1, status: 1)
+        @item_2 = create(:item, merchant: @merchant_1, status: 1)
+        @item_3 = create(:item, merchant: @merchant_1, status: 1)
+        @item_5 = create(:item, merchant: @merchant_1, status: 1)
+        @item_6 = create(:item, merchant: @merchant_1, status: 1)
+        @item_7 = create(:item, merchant: @merchant_1, status: 1)
+        @item_8 = create(:item, merchant: @merchant_1, status: 1)
+        @item_9 = create(:item, merchant: @merchant_1, status: 1)
+
+        @customer_1 = create(:customer)
+        @customer_2 = create(:customer)
+        @customer_3 = create(:customer)
+
+        @invoice_1 = create(:invoice, customer: @customer_1)
+        @invoice_2 = create(:invoice, customer: @customer_2)
+        @invoice_3 = create(:invoice, customer: @customer_3)
+        @invoice_4 = create(:invoice, customer: @customer_1)
+        @invoice_5 = create(:invoice, customer: @customer_2)
+        @invoice_6 = create(:invoice, customer: @customer_3)
+
+        @invoice_item_1 = create(:invoice_item, item: @item_1, invoice: @invoice_1, quantity: 10, unit_price: 10)
+        @invoice_item_2 = create(:invoice_item, item: @item_2, invoice: @invoice_2, quantity: 8, unit_price: 9)
+        @invoice_item_5 = create(:invoice_item, item: @item_5, invoice: @invoice_5, quantity: 5, unit_price: 5)
+        @invoice_item_4 = create(:invoice_item, item: @item_8, invoice: @invoice_4, quantity: 5, unit_price: 4)
+        @invoice_item_3 = create(:invoice_item, item: @item_3, invoice: @invoice_3, quantity: 3, unit_price: 3)
+        @invoice_item_6 = create(:invoice_item, item: @item_6, invoice: @invoice_6, quantity: 1, unit_price: 1)
+
+        @transaction_1 = create(:transaction, invoice: @invoice_1, result: 0)
+        @transaction_2 = create(:transaction, invoice: @invoice_2, result: 0)
+        @transaction_3 = create(:transaction, invoice: @invoice_5, result: 0)
+        @transaction_4 = create(:transaction, invoice: @invoice_4, result: 0)
+        @transaction_5 = create(:transaction, invoice: @invoice_3, result: 0)
+        @transaction_6 = create(:transaction, invoice: @invoice_6, result: 0)
+      end
+      it 'displays the top five items' do
+        expect(@merchant_1.top_five_items).to eq([@item_1, @item_2, @item_5, @item_8, @item_3])
       end
     end
   end
