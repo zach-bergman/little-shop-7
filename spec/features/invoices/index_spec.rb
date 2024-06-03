0804952e0d74a43e8d0d060608be2c92b83011a1
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-06-03 15:45:38 -0600

    test: merchant item index test setup

---
+++ spec/features/invoices/index_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'Invoices Index' do
+  before do
+    @merchant_1 = create(:merchant)
+    @merchant_2 = create(:merchant)
+
+    @customer_1 = create(:customer)
+    @customer_2 = create(:customer)
+
+    @item_1 = create(:item, merchant: @merchant_1, status: 1)
+    @item_2 = create(:item, merchant: @merchant_1, status: 1)
+    @item_3 = create(:item, merchant: @merchant_2, status: 1)
+
+    @invoice_1 = create(:invoice, customer: @customer_1)
+    @invoice_2 = create(:invoice, customer: @customer_2)
+
+    @invoice_item_1 = create(:invoice_item, item: @item_1, invoice: @invoice_1, quantity: 10, unit_price: 10)
+    @invoice_item_1 = create(:invoice_item, item: @item_2, invoice: @invoice_2, quantity: 10, unit_price: 10)
+    @invoice_item_1 = create(:invoice_item, item: @item_3, invoice: @invoice_1, quantity: 10, unit_price: 10)
+  end
+
+  describe 'as a merchant' do
+    it 'lists all invoices that include one of merchants items' do
+      visit merchant_invoices_path(@merchant_1)
+
+      within('.invoice-list') do
+        expect(page).to have_link("#{@invoice_1.id}")
+        expect(page).to have_link("#{@invoice_2.id}")
+        expect(page).to_not have_link("#{@invoice_3.id}")
+      end
+    end
+  end
+end
