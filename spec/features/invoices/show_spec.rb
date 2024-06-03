d056f3c21475ebd2d16421c78517e53838799b98
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-06-03 17:57:25 -0600

    feat: further testing on invoice items

--- spec/features/invoices/show_spec.rb
+++ spec/features/invoices/show_spec.rb
@@ -14,7 +14,6 @@ RSpec.describe 'invoice show' do
   describe 'as a merchant' do
     it 'can see the invoice information' do
       visit merchant_invoice_path(@merchant, @invoice_1)
-      save_and_open_page
 
       expect(page).to have_content(@invoice_1.id)
 
@@ -27,12 +26,28 @@ RSpec.describe 'invoice show' do
 
     it 'displays the items on the invoice for given merchant' do
       visit merchant_invoice_path(@merchant, @invoice_1)
-      save_and_open_page
+
       within('.invoice-items') do
         expect(page).to have_content(@item.name)
         expect(page).to have_content(@invoice_item_1.quantity)
         expect(page).to have_content(@invoice_item_1.price)
         expect(page).to have_content(@invoice_item_1.status)
+      end
+    end
+
+    it 'only displays information for items on invoice' do
+      item_2 = create(:item, merchant: @merchant)
+      @invoice_2 = create(:invoice, customer: @customer_1)
+      invoice_item_2 = create(:invoice_item, item: item_2, invoice: @invoice_2)
+
+      visit merchant_invoice_path(@merchant, @invoice_1)
+
+      within('.invoice-items') do
+        expect(page).to have_content(@item.name)
+        expect(page).to_not have_content(item_2.name)
+        expect(page).to_not have_content(invoice_item_2.quantity)
+        expect(page).to_not have_content(invoice_item_2.price)
+        expect(page).to_not have_content(invoice_item_2.status)
       end
     end
   end
