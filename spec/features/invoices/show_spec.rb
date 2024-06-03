d33762b05673dbee324a1ed765701dfa97656a11
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-06-03 17:24:51 -0600

    feat: fixed test and show page

--- spec/features/invoices/show_spec.rb
+++ spec/features/invoices/show_spec.rb
@@ -12,14 +12,17 @@ RSpec.describe 'invoice show' do
   end
 
   describe 'as a merchant' do
-    visit merchant_invoice_path(@merchant, @invoice_1)
+    it 'can see the invoice information' do
+      visit merchant_invoice_path(@merchant, @invoice_1)
+      save_and_open_page
 
-    expect(page).to have_content(@invoice_1.id)
+      expect(page).to have_content(@invoice_1.id)
 
-    within('.item-info') do
-      expect(page).to have_content("Status: #{invoice_1.status}")
-      expect(page).to have_content("Created at: #{invoice_1.formatted_date}")
-      expect(page).to have_content("Customer: #{invoice_1.customer.first_name} #{invoice_1.customer.last_name}")
+      within('.item-info') do
+        expect(page).to have_content("Status: #{@invoice_1.status}")
+        expect(page).to have_content("Created at: #{@invoice_1.format_date}")
+        expect(page).to have_content("Customer: #{@invoice_1.customer.first_name} #{@invoice_1.customer.last_name}")
+      end
     end
   end
 end
