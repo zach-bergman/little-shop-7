6a71f4776356df441cf6b75fe6386c7f6d0240b3
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-06-03 16:52:31 -0600

    feat: added view and controller action

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -3,4 +3,8 @@ class InvoicesController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @invoices = @merchant.invoices
   end
+
+  def show
+    @invoice = Invoice.find(params[:id])
+  end
 end
