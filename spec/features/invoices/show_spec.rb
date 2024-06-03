b1bf205ca2138f7c54326df1812db6c5d278e181
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-06-03 17:48:54 -0600

    test: items on invoice show

--- spec/features/invoices/show_spec.rb
+++ spec/features/invoices/show_spec.rb
@@ -18,10 +18,21 @@ RSpec.describe 'invoice show' do
 
       expect(page).to have_content(@invoice_1.id)
 
-      within('.item-info') do
+      within('.invoice-info') do
         expect(page).to have_content("Status: #{@invoice_1.status}")
         expect(page).to have_content("Created at: #{@invoice_1.format_date}")
         expect(page).to have_content("Customer: #{@invoice_1.customer.first_name} #{@invoice_1.customer.last_name}")
+      end
+    end
+
+    it 'displays the items on the invoice for given merchant' do
+      visit merchant_invoice_path(@merchant, @invoice_1)
+      save_and_open_page
+      within('.invoice-items') do
+        expect(page).to have_content(@item.name)
+        expect(page).to have_content(@invoice_item_1.quantity)
+        expect(page).to have_content(@invoice_item_1.price)
+        expect(page).to have_content(@invoice_item_1.status)
       end
     end
   end
