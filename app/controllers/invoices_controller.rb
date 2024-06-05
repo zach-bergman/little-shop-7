b1f08137ed187aba960f6d9466341d35ab9872bc
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-06-05 14:40:08 -0600

    feat: refactoring controllers

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -8,18 +8,4 @@ class InvoicesController < ApplicationController
     @invoice = Invoice.find(params[:id])
     @merchant = Merchant.find(params[:merchant_id])
   end
-
-  def update 
-    invoice = Invoice.find(params[:id])
-    merchant = Merchant.find(params[:merchant_id])
-    invoice.update(invoice_params)
-    flash[:notice] = 'Invoice status updated'
-    redirect_to merchant_invoice_path(merchant, invoice)
-  end
-
-  private
-
-  def invoice_params
-    params.permit(:status)
-  end
 end
