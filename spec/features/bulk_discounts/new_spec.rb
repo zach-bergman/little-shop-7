80d45f6394842b5a172df55de41c260d2f2a853e
Zach Bergman <bzach634@gmail.com>
2024-06-07 07:23:28 -0600

    test: Add assertion for error message

--- spec/features/bulk_discounts/new_spec.rb
+++ spec/features/bulk_discounts/new_spec.rb
@@ -75,10 +75,11 @@ RSpec.describe "Merchant Bulk Discount New Page" do
 
         fill_in("Name", with: "30%-15")
         fill_in("Percentage Discount", with: 30)
-        fill_in("Quantity Threshold", with: "fifteen")
+        fill_in("Quantity Threshold", with: "15")
         click_button("Submit")
 
         expect(current_path).to eq(new_merchant_bulk_discount_path(merchant_1.id))
+        expect(page).to have_content("Error: Name has already been taken, Percentage has already been taken, and Quantity threshold has already been taken")
       end
     end
   end
