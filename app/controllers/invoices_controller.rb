a8b7b7968c3b551ef7dedd90a76f23b772a0e619
Zach Bergman <bzach634@gmail.com>
2024-06-09 10:04:05 -0600

    feat: Add instance variables for Total Revenue and Total Discounted Revenue to show action

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -7,5 +7,7 @@ class InvoicesController < ApplicationController
   def show
     @invoice = Invoice.find(params[:id])
     @merchant = Merchant.find(params[:merchant_id])
+    @total_revenue = @merchant.total_revenue_for_invoice(@invoice)
+    @total_discounted_revenue = @merchant.total_discounted_revenue_for_invoice(@invoice)
   end
 end
