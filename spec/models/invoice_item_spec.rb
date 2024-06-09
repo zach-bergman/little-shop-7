3fa08dd950a901b4efa489f78d1248386927755e
Zach Bergman <bzach634@gmail.com>
2024-06-09 15:07:05 -0600

    General editing

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe InvoiceItem, type: :model do
     it { should belong_to :item }
     it { should belong_to :invoice }
     it { should have_one :merchant }
+    it { should have_many(:bulk_discounts).through(:merchant) }
   end
 
   describe 'enums' do
