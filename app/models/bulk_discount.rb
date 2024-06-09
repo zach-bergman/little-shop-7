69d3ebbe01ee844714e3bdc7cfc057314a15c820
Zach Bergman <bzach634@gmail.com>
2024-06-09 10:04:49 -0600

    refactor: Change validations to not be unique

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,8 +1,7 @@
-#change name of table to bulk_discounts?
 class BulkDiscount < ApplicationRecord
   belongs_to :merchant
 
   validates :name, presence: true, uniqueness: true
-  validates :percentage, presence: true, numericality: true, uniqueness: true
-  validates :quantity_threshold, presence: true, numericality: true, uniqueness: true
+  validates :percentage, presence: true, numericality: true
+  validates :quantity_threshold, presence: true, numericality: true
 end
