3c50e5ee56e455bf5a77ff49f0a9fbc9ad36614f
Zach Bergman <bzach634@gmail.com>
2024-06-09 15:06:35 -0600

    feat: Add invoices association to BulkDiscount model

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,5 +1,6 @@
 class BulkDiscount < ApplicationRecord
   belongs_to :merchant
+  has_many :invoices
 
   validates :name, presence: true, uniqueness: true
   validates :percentage, presence: true, numericality: true
