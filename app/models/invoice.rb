6bbdc6c3aca3a0f6a5b3be79bf46d0505300a596
Zach Bergman <bzach634@gmail.com>
2024-06-10 14:46:06 -0600

    Move methods to Invoice instead of Merchant

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -25,4 +25,20 @@ class Invoice < ApplicationRecord
     total = invoice_items.sum('unit_price * quantity')
     number_to_currency(total / 100.0)
   end
+
+  def total_revenue_for_invoice(merchant)
+    merchant.invoice_items
+          .joins(:item)
+          .where(items: { merchant_id: merchant.id })
+          .sum("invoice_items.quantity * invoice_items.unit_price")
+  end
+
+  def total_discounted_revenue_for_invoice
+      invoice_items
+        .joins(item: :merchant)
+        .joins("LEFT JOIN bulk_discounts ON bulk_discounts.merchant_id = items.merchant_id AND invoice_items.quantity >= bulk_discounts.quantity_threshold")
+        .select('invoice_items.id, invoice_items.quantity, invoice_items.unit_price, COALESCE(MAX(bulk_discounts.percentage), 0) AS discount')
+        .group('invoice_items.id')
+        .sum { |ii| ii.unit_price * ii.quantity * (1 - ii.discount / 100.0) }
+  end
 end
