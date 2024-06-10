8288f96e7a2f2c2dd804dd99bb4f83bd201993cd
Zach Bergman <bzach634@gmail.com>
2024-06-10 14:53:04 -0600

    refactor: Change instance variable to use invoice method

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -7,8 +7,7 @@ class InvoicesController < ApplicationController
   def show
     @invoice = Invoice.find(params[:id])
     @merchant = Merchant.find(params[:merchant_id])
-    @total_revenue = @merchant.total_revenue_for_invoice(@invoice)
-    @total_discounted_revenue = @merchant.total_discounted_revenue_for_invoice(@invoice)
+    @total_discounted_revenue_for_merchant = @invoice.total_discounted_revenue_for_invoice
     @bulk_discounts = @merchant.bulk_discounts
   end
 end
